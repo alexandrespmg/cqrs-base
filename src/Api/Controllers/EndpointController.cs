daa33d7bf542e14e2125ac6ecc2b14040f13bd7c
Alexadre <alexandrespmg@hotmail.com>
2020-01-27 09:39:00 -0300

    Update defaults endpoints

---
+++ src/Api/Controllers/EndpointController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EndpointController : ControllerBase
+    {
+        private readonly ILogger<EndpointController> _logger;
+
+        public EndpointController(ILogger<EndpointController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello";
+        }
+    }
+}
